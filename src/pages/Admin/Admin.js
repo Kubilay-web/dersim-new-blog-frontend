83ae6e11a0c4302bfcaddfea10944dc91bf349af
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2025-02-05 15:57:46 +0300

    Updated

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -297,7 +297,7 @@ function Admin() {
               <div className="d-sm-flex align-items-center justify-content-between mb-4">
                 <h1 className="h3 mb-0 text-gray-800">
                   {currentPage === "dashboard"
-                    ? ""
+                    ? "Dashboard"
                     : currentPage === "payment-manage"
                     ? "Payment Management"
                     : currentPage === "product-manage"
